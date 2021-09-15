92bfdbca188b6a8c946c077d936a11565365f009
youssefboussaid <youssbsd@gmail.com>
2021-09-15 07:20:16 -0700

    task12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,14 +4,33 @@ def roman_to_int(roman_string):
     elif not roman_string:
         return 0
     else:
-        R = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000, 'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900}
-        i = 0
-        tot = 0
-        while i < len(roman_string):
-            if i + 1 < len(roman_string) and roman_string[i: i + 2] in R:
-                tot += R[roman_string[i: i + 2]]
-                i += 2
-            else:
-                tot += R[roman_string[i]]
-                i += 1
-        return tot
+        value = {
+        'M': 1000,
+        'D': 500,
+        'C': 100,
+        'L': 50,
+        'X': 10,
+        'V': 5,
+        'I': 1
+    }
+ 
+    # Initialize previous character and answer
+    p = 0
+    ans = 0
+ 
+    # Traverse through all characters
+    n = len(roman_string)
+    for i in range(n-1, -1, -1):
+ 
+        # If greater than or equal to previous,
+        # add to answer
+        if value[roman_string[i]] >= p:
+            ans += value[roman_string[i]]
+ 
+        # If smaller than previous
+        else:
+            ans -= value[roman_string[i]]
+ 
+        # Update previous
+        p = value[roman_string[i]]
+    return(ans)
