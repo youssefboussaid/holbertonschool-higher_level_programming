142b9c44f4405044e00e8fff145de69fcb0c227d
youssefboussaid <youssbsd@gmail.com>
2021-09-27 04:34:03 -0700

    task0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,13 +2,15 @@
 """
     prints x elements of a list.
     """
+
+
 def safe_print_list(my_list=[], x=0):
     nb = 0
-    for x in range(0 ,x):
+    for x in range(0, x):
         try:
             print(my_list[x], end="")
             nb += 1
-        except:
+        except BaseException:
             break
     print("")
     return(nb)
