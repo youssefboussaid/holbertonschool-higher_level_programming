18fa8cd70e04fb7850f419b394918b1d26f4211e
youssefboussaid <youssbsd@gmail.com>
2021-09-27 04:28:54 -0700

    task0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    nb = 0
+    for x in range(0 ,x):
+        try:
+            print(my_list[x], end="")
+            nb += 1
+        except:
+            break
+    print("")
+    return(nb)
