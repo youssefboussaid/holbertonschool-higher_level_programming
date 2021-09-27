bdfb396b251cdfbef5eb69243e589a481b8af5e7
youssefboussaid <youssbsd@gmail.com>
2021-09-27 04:36:57 -0700

    task2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    nb = 0
+    for y in range(0, x):
+        try:
+            print("{:d}".format(my_list[y]), end="")
+            nb += 1
+        except(TypeError, ValueError):
+            pass
+    print()
+    return(nb)
