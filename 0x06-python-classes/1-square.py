612f8daf6d50d3212e97a1b16dbc38fd54877f13
youssefboussaid <youssbsd@gmail.com>
2021-09-28 11:20:49 -0700

    task1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""class square
+"""
+
+
+class Square:
+    """class Square that defines a square
+        Attributes:
+            size (int): square size
+    """
+
+    def __init__(self, size):
+        """
+        initialization methode
+        Args:
+        size (int) : square size
+        """
+        self.__size = size
