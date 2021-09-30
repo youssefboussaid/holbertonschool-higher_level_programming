b230f3b4aa72b6c1fbde97a2a6af11686b30a3df
youssefboussaid <youssbsd@gmail.com>
2021-09-30 08:02:49 -0700

    task100

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,5 +7,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except Exception as err:
-        print("Execption {}".format(err), file=sys.stderr)
+        print("Exception {}".format(err), file=sys.stderr)
         return False
