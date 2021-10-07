81d92c977af2530cbcdae7b9abf52342b8b2de9a
youssefboussaid <85492375+youssefboussaid@users.noreply.github.com>
2021-10-07 13:25:39 +0200

    Update 5-text_indentation.py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -6,12 +6,10 @@ Module to print a text with 2 new lines
 
 def text_indentation(text):
     """
-     a function that prints a text with 2 new lines/
+    a function that prints a text with 2 new lines/
     after each of these characters: ., ? and :
     Args:
         text(str): the given text
-    Raises:
-        TypeError: if the text is not str
     Returns:
         the new text
     """
@@ -25,5 +23,7 @@ def text_indentation(text):
             if i == len(text) - 1:
                 break
             if text[i + 1] == " ":
+                i += 1
+            while text[i] == ' ' and text[i + 1] == ' ' and i + 1 < len(text):
                 i += 1
         i += 1
