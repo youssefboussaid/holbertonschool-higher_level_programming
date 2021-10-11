1af017e3824333c573964776e06b7e5e88609b4f
youssefboussaid <youssbsd@gmail.com>
2021-10-11 01:21:03 -1100

    task4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" inherits_from module"""
+def inherits_from(obj, a_class):
+    """function to verifie subclass"""
+    if issubclass(type(obj), a_class) and type(obj)!= a_class:
+        return True
+    else:
+        return False
