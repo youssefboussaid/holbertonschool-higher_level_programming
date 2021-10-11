1c71a38102c9e30dd8ca21f0f97a10e3ef4383f3
youssefboussaid <youssbsd@gmail.com>
2021-10-11 01:00:40 -1100

    task2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""compare obj to instance"""
+
+def is_same_class(obj, a_class):
+    """function to comapre obj to instance"""
+    if type (obj) == a_class:
+        return True
+    else:
+        return False
