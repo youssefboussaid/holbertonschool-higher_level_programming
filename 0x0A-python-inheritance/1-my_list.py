688f840dcb8a9ea3aad7b98cb38a10f15cd03e80
youssefboussaid <youssbsd@gmail.com>
2021-10-11 00:47:23 -1100

    task1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""MyList module"""
 
 class MyList(list):
+    """MyList class"""
     def print_sorted(self):
+        """
+        print a sorted list
+        """
         print(sorted(self))
