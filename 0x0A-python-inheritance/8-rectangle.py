448b26fdf215f0ccc4112ca273d42d4cc52194a3
youssefboussaid <youssbsd@gmail.com>
2021-10-11 01:53:53 -1100

    task8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Rectangle module"""
+
+class Rectangle(BaseGeometry):
+    """Rectangle class"""
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = __height
