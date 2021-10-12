b10b01d93b80635bf7f207ddf865c8e306a7ea7b
youssefboussaid <youssbsd@gmail.com>
2021-10-11 21:18:56 -1100

    task100

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 """
-MyInt module 
+MyInt module
 """
+
 
 class MyInt(int):
     """
     MyInt class
     """
+
     def __eq__(self, value):
         """ equal"""
         return not super().__eq__(value)
+
     def __ne__(self, value):
         """ diffrent """
         return not super().__ne__(value)
