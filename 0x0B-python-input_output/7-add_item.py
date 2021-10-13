2908d84fc0acf9a8885033f483c4b465aadcfa9b
youssefboussaid <youssbsd@gmail.com>
2021-10-12 19:15:02 -1100

    task7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""module to add to json file
+"""
+import sys
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+l = []
+open(add_item.json, "a")
+try:
+    l = load_from_json_file("add_item.json")
+except Exception:
+    l = []
+save_to_json_file(l + sys.argv[1:], "add_item.json")
