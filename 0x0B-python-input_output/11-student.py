5bd4f8465ba7f8ba4a8ceb2183afefe0018c48aa
youssefboussaid <youssbsd@gmail.com>
2021-10-12 19:39:34 -1100

    task11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -3,8 +3,10 @@
 module to define Student class
 """
 
+
 class Student():
     """ Student class"""
+
     def __init__(self, first_name, last_name, age):
         """ init methode"""
         self.first_name = first_name
@@ -13,7 +15,8 @@ class Student():
 
     def to_json(self, attrs=None):
         """dict representation"""
-        if type(attrs) is list and all([type(x) == str for x in attrs]):
+        if isinstance(attrs, list) and all(
+                [isinstance(x, str) for x in attrs]):
             return {k: v for k, v in self.__dict__.items() if k in attrs}
         return(self.__dict__)
 
@@ -21,3 +24,4 @@ class Student():
         """ replace attributes of Student instance"""
         for i, j in json.items():
             self.__dict__[i] = j
+                                     
