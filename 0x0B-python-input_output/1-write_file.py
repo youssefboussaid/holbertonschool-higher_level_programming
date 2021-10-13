df9a82472435ba50c4ac817ffd7f992c01411b27
youssefboussaid <youssbsd@gmail.com>
2021-10-12 18:52:11 -1100

    task1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -8,5 +8,7 @@ def read_file(filename=""):
     """
     func reads a text file (UTF8) and prints it to stdout
     """
-    with open(filename, "r", encoding="utf-8") as filename:
-        print(filename.read(), end="")
+    with open(filename, "w", encoding="utf-8") as filename:
+        filename.write(text)
+    return len(text)
+    
