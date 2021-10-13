0c6689cb57f469496860311541375e82241088b3
youssefboussaid <youssbsd@gmail.com>
2021-10-12 19:13:54 -1100

    task6

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -4,6 +4,7 @@ module to load from a json file
 """
 import json
 
+
 def load_from_json_file(filename):
     """func that loads from a json file"""
     with open(filename, "r", encoding="utf-8") as filename:
