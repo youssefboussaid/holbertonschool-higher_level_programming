dc80ca18568f200c9976953e565a9edab6d39560
youssefboussaid <youssbsd@gmail.com>
2021-10-18 18:56:24 -1100

    rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -14,66 +14,66 @@ class Rectangle(Base):
         self.width = width
         self.height = height
         self.x = x
-        self.y = y
+        self.y = y  
 
     @property
     def width(self):
-        """ width getter"""
+        """ width getter """
         return self.__width
+
+    @width.setter
+    def width(self, w):
+        """ width setter """
+        if type(w) is not int:
+            raise TypeError("width must be an integer")
+        if w <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.__width = w
 
     @property
     def height(self):
-        """height getter"""
+        """ height getter """
         return self.__height
+
+    @height.setter
+    def height(self, h):
+        """ height setter """
+        if type(h) is not int:
+            raise TypeError("height must be an integer")
+        if h <= 0:
+            raise ValueError("height must be > 0")
+        else:
+            self.__height = h
 
     @property
     def x(self):
-        """x getter"""
+        """ x getter """
         return self.__x
+
+    @x.setter
+    def x(self, x):
+        """ x setter """
+        if type(x) is not int:
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must be >= 0")
+        else:
+            self.__x = x
 
     @property
     def y(self):
-        """y getter"""
+        """ y getter """
         return self.__y
 
-    @width.setter
-    def width(self, value):
-        """width setter"""
-        if not isinstance(value, int):
-            raise TypeError("width must be an interger")
-        if value <= 0:
-            raise ValueError("width must be > 0")
-        else:
-            self.__width = value
-
-    @height.setter
-    def height(self, value):
-        """height setter"""
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value <= 0:
-            raise ValueError("height must be > 0")
-        else:
-            self.__height = value
-
-    @x.setter
-    def x(self, value):
-        """y setter"""
-        if not isinstance(value, int):
-            raise TypeError("x must be an integer")
-        if value < 0:
-            raise ValueError("x must be >= 0")
-        else:
-            self.__x = value
-
     @y.setter
-    def y(self, value):
-        """y setter"""
-        if not isinstance(value, int):
+    def y(self, y):
+        """ y setter """
+        if type(y) is not int:
             raise TypeError("y must be an integer")
-        if value < 0:
+        if y < 0:
             raise ValueError("y must be >= 0")
-        self.__y = value
+        self.__y = y
 
     def area(self):
         """area methode"""
