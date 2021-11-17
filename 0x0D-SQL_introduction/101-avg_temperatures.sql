02da0e702bb314486d8fe9ecf5357cbf1ea067e1
root <root@DESKTOP-8SJSIIJ.localdomain>
2021-11-16 22:48:39 -1100

    task

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,2 @@
+-- displays the average temperature (Fahrenheit) by city ordered by temperature (descending).
+SELECT city, AVR(value) AS "avg_temp" FROM temperature GROUP BY city ORDER BY avg_temp DESC;
---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,2 @@
+-- displays the top 3 of cities temperature during July and August ordered by temperature
+SELECT city, AVR(value) AS avg_temp FROM temperatures WHERE month=7 OR month=8 GROUP BY avg_temp DESC LIMIT 3;
