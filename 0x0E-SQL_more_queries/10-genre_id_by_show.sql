1395bf3432ce4c4a31060f65bd2885b33ced25c5
root <root@DESKTOP-8SJSIIJ.localdomain>
2021-11-17 21:24:25 -1100

    task

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,4 @@
 -- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
 SELECT tv_show.title, tv_show.genres.genre_id
-FROM tv_show_genres INNER JOIN tv_show ON tv_show_genres.show_id = tv_show.id
+FROM tv_show_genres INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
