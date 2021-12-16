271e883802690385b500816de4359365f6fbdf36
youssefboussaid <youssbsd@gmail.com>
2021-12-16 11:22:28 -1100

    task

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const args = process.argv[2];
-if (isNaN(args)){
-    console.log('Not a number');
+if (isNaN(args)) {
+  console.log('Not a number');
 } else {
-    console.log('My number: %d', args);
+  console.log('My number: %d', args);
 }
