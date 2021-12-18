8cb10e6cd24a63285d1a9bc55ee043e322dff350
youssefboussaid <youssbsd@gmail.com>
2021-12-17 19:09:25 -1100

    task

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
   let count = 0;
-  for (let i = 0; i < list.length; i++) {
+  for (const i in list) {
     if (list[i] == searchElement) {
       count++;
     }
