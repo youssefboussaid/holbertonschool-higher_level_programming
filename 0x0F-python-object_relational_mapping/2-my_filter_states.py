32411c1092dd77eae01aba494a9c52b59f431746
youssefboussaid <youssbsd@gmail.com>
2021-12-19 10:15:28 -1100

    task

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,23 +5,19 @@ Write a script that lists all states from the database hbtn_0e_0_usa:
 import MySQLdb
 from sys import argv
 if __name__ == "__main__":
-    # Open database connection
+
     db = MySQLdb.connect(
         host="localhost",
+        port=3306,
         user=argv[1],
         password=argv[2],
         database=argv[3],
     )
-    # prepare a cursor object using cursor() method
     cursor = db.cursor()
-    # sql query string to be executed on the database
-    sql = "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id ASC".format(
-        argv[4])
-    # Execute the SQL command
-    cursor.execute(sql)
-    # Fetch all the rows in a list of lists.
+    cursor.execute(
+        "SELECT * FROM states WHERE Name LIKE BINARY '{}' ORDER BY id ASC"
+        .format(argv[4]))
     results = cursor.fetchall()
-    # Now print fetched result
     for row in results:
         print(row)
     cursor.close()
