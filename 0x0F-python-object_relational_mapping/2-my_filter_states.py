bf09b1dbe805a24c0d2c30bcded795e452b3910d
youssefboussaid <youssbsd@gmail.com>
2021-12-19 10:14:27 -1100

    task

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,7 +5,6 @@ Write a script that lists all states from the database hbtn_0e_0_usa:
 import MySQLdb
 from sys import argv
 if __name__ == "__main__":
-
     # Open database connection
     db = MySQLdb.connect(
         host="localhost",
