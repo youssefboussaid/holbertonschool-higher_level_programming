b5b91013f46031ac6af4b51a7543787cecfdd467
youssefboussaid <youssbsd@gmail.com>
2021-12-19 10:37:38 -1100

    task

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -10,6 +10,12 @@ Base = declarative_base()
 
 
 class State(Base):
+     """
+    Represents a state for a MySQL database.
+    __tablename__ (str): The name of the MySQL table to store States.
+    id (sqlalchemy.Integer): The state's id.
+    name (sqlalchemy.String): The state's name.
+    """
     __tablename__ = "states"
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
