1996bd292994320d7d561ab62b5825f9849ded2e
youssefboussaid <youssbsd@gmail.com>
2021-12-19 10:04:32 -1100

    task

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,9 +2,9 @@
 """
 Write a script that lists all states from the database hbtn_0e_0_usa:
 """
+import MySQLdb
+from sys import argv
 if __name__ == "__main__":
-    import MySQLdb
-    from sys import argv
     # Open database connection
     db = MySQLdb.connect(
         host="localhost",
