84f8038b5af0e51d4b9f70dd53216dcb8a9f5751
youssefboussaid <youssbsd@gmail.com>
2022-01-08 19:31:10 -1100

    task

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes URL, sends POST request, display body
-curl -s -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
+curl -s -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" -X POST "$1"
