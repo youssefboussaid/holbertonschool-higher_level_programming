98576ab31e464bf5f1cf3a4325c8710db34d909f
youssefboussaid <youssbsd@gmail.com>
2022-01-09 12:59:48 -1100

    task

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,9 +4,10 @@
 from urllib import request
 
 if __name__ == "__main__":
+
     with request.urlopen("https://intranet.hbtn.io/status") as response:
-        content = response.read()
         print("Body response:")
+        content = response.read()
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
         print("\t- utf8 content: {}".format(content.decode('utf-8')))
