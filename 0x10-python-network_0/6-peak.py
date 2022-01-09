7bb9e26901514f80262f6d8aee24fcdc30b7f1ae
youssefboussaid <youssbsd@gmail.com>
2022-01-09 12:09:30 -1100

    task

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,15 +4,10 @@ peak function
 """
 
 def find_peak(list_of_integers):
-    len_list = len(list_of_integers)
-    if list_of_integers == []:
-        return None
-    if len_list == 1:
-        return list_of_integers[0]
-    if list_of_integers[0] >= list_of_integers[1]:
-        return list_of_integers[0]
-    if list_of_integers[len(list_of_integers) - 1] >= list_of_integers[len(list_of_integers) - 2]:
-        return list_of_integers[len(list_of_integers) - 1]
-    for i in range(1, len(list_of_integers) - 1):
-        if list_of_integers[i] >= list_of_integers[i - 1] and list_of_integers[i] >= list_of_integers[i + 1]:
-            return list_of_integers[i]
+    """BRUTE force implementation for question
+    """
+    max_i = None
+    for ele in list_of_integers:
+        if max_i is None or max_i < ele:
+            max_i = ele
+    return max_i
