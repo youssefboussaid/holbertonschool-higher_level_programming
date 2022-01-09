b1e1dc4e3ce5ddb3dce2cd5f8202ae2aab768517
youssefboussaid <youssbsd@gmail.com>
2022-01-08 19:20:57 -1100

    task

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -sLX GET $1
+if [ $(curl -L -s -X HEAD -w "%{http_code}" "$1") == '200' ]; then curl -Ls "$1"; fi
