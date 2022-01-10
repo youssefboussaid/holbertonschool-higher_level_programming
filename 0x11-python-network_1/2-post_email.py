e180d9ad77e8fde41fc0040578a5cf959477476c
youssefboussaid <youssbsd@gmail.com>
2022-01-09 18:34:20 -1100

    task

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -3,10 +3,13 @@
 request to the passed URL with the email as a parameter, and
 displays the body of the response (decoded in utf-8)"""
 
-from urllib import request, parse
-from sys import argv
+import sys
+import urllib.request
+import urllib.parse
 
 if __name__ == "__main__":
-    data = parse.urlencode({"email": argv[2]}).encode('utf-8')
-    with request.urlopen(argv[1], data) as page:
-        print(page.read().decode('utf-8'))
+    values = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('utf-8')
+    with urllib.request.urlopen("http://0.0.0.0:5000/post_email", data) as req:
+        print(req.read().decode('utf-8'))
