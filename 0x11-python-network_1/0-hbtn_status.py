415c5cdcb6fa1ffb6d2e47ff8596a248af2e427e
youssefboussaid <youssbsd@gmail.com>
2022-01-09 13:00:59 -1100

    task

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-"""Python script that fetches https://intranet.hbtn.io/status."""
+"""
+Python script that fetches
+https://intranet.hbtn.io/status
+"""
+import urllib.request
 
-from urllib import request
+if __name__ == '__main__':
 
-if __name__ == "__main__":
-
-    with request.urlopen("https://intranet.hbtn.io/status") as response:
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         print("Body response:")
-        content = response.read()
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+        html = response.read()
+        print("\t- type:", type(html))
+        print("\t- content:", html)
+        print("\t- utf8 content:", html.decode('utf-8'))
