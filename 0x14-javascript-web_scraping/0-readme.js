278b4297bf43ff7400d3cc5577cddcd83ac47f97
youssefboussaid <youssbsd@gmail.com>
2022-02-01 13:19:12 -1100

    task

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,10 +3,10 @@
 const argv = process.argv;
 const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf8', (err, data) => {
-    if (err) {
-        console.log(err)
-    } else {
-        console.log(data);
-    }
+fs.readFile(argv[2], 'utf8', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
 });
